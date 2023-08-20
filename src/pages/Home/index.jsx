a004e9009e3ebdecb26e4eaa8fb37c8b70b2f705
ElvisGalvez <galvez.elvis@gmail.com>
2023-08-20 17:40:26 +0200

    Correction d'un bug de loop et de filtrage sur le Mimic

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Header from '../../components/Header';
 import CardsContainer from '../../components/CardsContainer';
 import EvidenceFilter from '../../components/EvidenceFilter';
@@ -8,23 +8,31 @@ function Home() {
   const [filteredEntities, setFilteredEntities] = useState(entitiesData.entités);
   const [selectedEvidences, setSelectedEvidences] = useState([]);
 
-  const handleFilterChange = (evidenceState) => {
+  const handleFilterChange = useCallback((evidenceState) => {
     const confirmed = Object.keys(evidenceState).filter(k => evidenceState[k] === 'confirmed');
     const refuted = Object.keys(evidenceState).filter(k => evidenceState[k] === 'refuted');
-    
+
     const isMimic = (entity) => {
-      return entity.nom === 'Mimic'; 
+        return entity.nom === 'Mimic';
     };
+
+    // Si aucune preuve n'est confirmée ou réfutée, retourne tous les fantômes
+    if (confirmed.length === 0 && refuted.length === 0) {
+        setFilteredEntities(entitiesData.entités);
+        setSelectedEvidences([]);
+        return;  // Sortie anticipée
+    }
 
     const newFilteredEntities = entitiesData.entités.filter(entity => {
       // Pour le Mimic : 
       if (isMimic(entity)) {
-        const mimicOrbeDetected = confirmed.includes("Orbe fantomatique");
-        const hasAllMimicEvidence = ["Spirit Box", "Ultraviolet", "Températures glaciales"].every(e => confirmed.includes(e));
-        const hasRefutedMimicEvidence = refuted.some(e => ["Spirit Box", "Ultraviolet", "Températures glaciales", "Orbe fantomatique"].includes(e));
+        const mimicEvidences = ["Spirit Box", "Ultraviolet", "Températures glaciales", "Orbe fantomatique"];
+        const hasAnyMimicEvidence = mimicEvidences.some(e => confirmed.includes(e));
+        const hasConfirmedNonMimicEvidence = confirmed.some(e => !mimicEvidences.includes(e));
+        const hasRefutedMimicEvidence = refuted.some(e => mimicEvidences.includes(e));
 
-        return (mimicOrbeDetected && hasAllMimicEvidence) && !hasRefutedMimicEvidence;
-      }
+        return hasAnyMimicEvidence && !hasConfirmedNonMimicEvidence && !hasRefutedMimicEvidence;
+      } 
 
       // Pour les autres fantômes :
       const hasAllConfirmed = confirmed.every(e => entity.preuves.includes(e));
@@ -32,10 +40,11 @@ function Home() {
 
       return hasAllConfirmed && !hasAnyRefuted;
     });
-    
+
     setFilteredEntities(newFilteredEntities);
     setSelectedEvidences(confirmed);
-};
+}, []);
+
 
 
   return (
